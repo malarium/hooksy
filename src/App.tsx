c6d881c1062588b112ca59315dbb07386969666d
marcin.gorecki <marcingorecki1981@gmail.com>
2022-01-21 10:30:30 +0100

    sort hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,46 @@
 import React, { MutableRefObject } from "react";
 import useGetDimensions from "./lib/useGetDimensions";
+import useOnMount from "./lib/useOnMount";
 import useOnUnmount from "./lib/useOnUnmount";
+import useSortAplhabetically from "./lib/useSortAplphabetically";
 
-function test(text: string) {
-  alert(text);
+function logInConsole(data: any) {
+  console.log(data);
 }
 
 function App() {
-  useOnUnmount(() => test("end"));
   const button: MutableRefObject<any> = React.useRef();
+  const sampleArrayOfStrings: string[] = [
+    "these",
+    "strings",
+    "should",
+    "be",
+    "sorted",
+    "alphabetically",
+    "Yoda",
+    "style",
+  ];
 
-  function useRunTESTS() {
-    const { width, height } = useGetDimensions(button);
-    alert(`works:  ${width}px ${height}px`);
+  const sampleArrayOfStringsSorted =
+    useSortAplhabetically(sampleArrayOfStrings);
+
+  useOnMount(() => {
+    logInConsole("rendered");
+    logInConsole(sampleArrayOfStringsSorted);
+  });
+  useOnUnmount(() => logInConsole("unmounted"));
+
+  function useGetDimensionsTest() {
+    const { width, height, left, top } = useGetDimensions(button);
+    logInConsole(
+      `width:  ${width}px, height: ${height}px, left: ${left}px, top: ${top}px `
+    );
   }
 
   return (
     <div className="App">
       <p>TEST</p>
-      <button ref={button} onClick={useRunTESTS}>
+      <button ref={button} onClick={useGetDimensionsTest}>
         CLICK
       </button>
     </div>
