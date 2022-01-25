29c878940e2f507c08e69ecc99d77a71ceb2aa2f
marcin.gorecki <marcingorecki1981@gmail.com>
2022-01-25 19:06:18 +0100

    Build generated

---
+++ dist/helpers/interfaces.ts
@@ -0,0 +1,46 @@
+declare type FillMode = "none" | "forwards" | "backwards" | "both" | "auto";
+declare type IterationCompositeOperation = "replace" | "accumulate";
+declare type EasingMode =
+  | "linear"
+  | "ease-in"
+  | "ease-out"
+  | "ease-in-out"
+  | "linear"
+  | "step-start"
+  | "step-end";
+declare type PlaybackDirection =
+  | "normal"
+  | "reverse"
+  | "alternate"
+  | "alternate-reverse";
+declare type AnimationPlayState =
+  | "idle"
+  | "pending"
+  | "running"
+  | "paused"
+  | "finished";
+declare type CompositeOperation = "replace" | "add" | "accumulate";
+declare type DOMHighResTimeStamp = number;
+
+interface IBaseAnimation {
+  element: React.MutableRefObject<any>;
+  duration?: number;
+  fill?: FillMode;
+  easing?: EasingMode;
+  iterations?: number;
+}
+export interface Imove extends IBaseAnimation {
+  x: number;
+  y: number;
+  unit?: `px` | `%`;
+}
+
+export interface Iresize extends IBaseAnimation {
+  scale: number;
+  axis?: `X` | `Y`;
+}
+
+export interface Irotate extends IBaseAnimation {
+  turnDegree: number;
+  unit?: `deg` | `turn` | `rad`;
+}
---
+++ dist/useAnimation.ts
@@ -0,0 +1,52 @@
+import { Imove, Iresize, Irotate } from "./helpers/interfaces";
+// TODO: Add 'combine' method that would chain animations with the same action (transform) - is that needed?
+// TODO: these can't be arrays - create interface and accept objects
+
+const useAnimation = () => {
+  const generateDefaultTimingOptions = (data: Imove | Iresize | Irotate) => {
+    return {
+      duration: data.duration || 500,
+      fill: data.fill || `none`,
+      easing: data.easing || `linear`,
+      iterations: data.iterations || 1,
+    };
+  };
+  const move = (moveData: Imove) => {
+    const animation = [
+      {
+        transform: `translate(${moveData.x}${moveData.unit || `%`}, ${
+          moveData.y
+        }${moveData.unit || `%`})`,
+      },
+    ];
+    const animationTiming = generateDefaultTimingOptions(moveData);
+    console.log(`Animation: `, animation);
+    return moveData.element.current.animate([...animation], animationTiming);
+  };
+
+  const resize = (resizeData: Iresize) => {
+    const scaleOption: string = `scale${resizeData.axis || ``}`;
+    const animation = [
+      {
+        transform: `${scaleOption}(${resizeData.scale})`,
+      },
+    ];
+    const animationTiming = generateDefaultTimingOptions(resizeData);
+    return resizeData.element.current.animate(animation, animationTiming);
+  };
+  const rotate = (rotateData: Irotate) => {
+    const animation = [
+      {
+        transform: `rotate(${rotateData.turnDegree}${
+          rotateData.unit || `deg`
+        })`,
+      },
+    ];
+    const animationTiming = generateDefaultTimingOptions(rotateData);
+    return rotateData.element.current.animate(animation, animationTiming);
+  };
+
+  return { move, resize, rotate };
+};
+
+export default useAnimation;
